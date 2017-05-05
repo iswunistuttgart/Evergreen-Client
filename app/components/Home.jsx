9246ab38f213a901cdc049809a020409e90b2071
Shanseevan Kanagaratnam <shanjiv@hotmail.com>
2017-05-05 02:47:37 +0200

    Add Overview page as Home component

---
+++ app/components/Home.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+
+  componentDidMount() {
+  }
+  render() {
+    return (
+      <div>
+        this is home
+      </div>
+    );
+  }
+}
+
+export default Home;
