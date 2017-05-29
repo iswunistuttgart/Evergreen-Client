109cd0cba962a5a523867a064676e51c06cfddd4
Shanseevan Kanagaratnam <shanjiv@hotmail.com>
2017-05-29 15:22:52 +0200

    Fixing performance of linechart

--- app/components/SimpleLineChart.jsx
+++ app/components/SimpleLineChart.jsx
@@ -1,35 +1,63 @@
 import React, {Component} from 'react';
 import {browserHistory} from 'react-router';
 import d3 from 'd3';
-import Epoch from './EpochChart';
-
-let data = [
-  { label: 'Layer 1', values: [] }
-];
+require('./flot');
+require('./flottime');
 
 class SimpleLineChart extends Component {
 
   componentDidMount() {
-    this.areaChartInstance = $('#line' + this.props.id).epoch({
-        type: 'time.line',
-        data: data,
-        fps: 1, 
-        axes: ['left', 'right', 'bottom']
-    });
+		// We use an inline data source in the example, usually data would
+		// be fetched from a server
+
+    this.data = [];
+
+		this.plot = $.plot("#flotplaceholder" + this.props.id, [ this.data ], {
+			series: {
+				shadowSize: 0	// Drawing is faster without shadows
+			},
+			yaxis: {
+				show: true
+			},
+			xaxis: {
+				mode: "time",
+        timeformat: "%H:%M:%S",
+        show: true,
+        timezone: 'browser'
+			},
+      grid: {
+        hoverable: true,
+        clickable: true
+      }
+		});
+
+    this.myInterval = setInterval(() => {
+      this.plot.setData([this.data])
+      this.plot.setupGrid();
+
+      this.plot.draw();
+    }, 100)
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.value !== nextProps.value) {
-      this.areaChartInstance.push([{y: nextProps.value, time: ((new Date()).getTime() / 1000)|0}]);
+      if (this.data.length > 100) {
+        this.data.shift();
+      }
+      let newDate = (new Date()).getTime();
+      this.data.push([newDate, nextProps.value])
     }
   }
 
   componentWillUnmount() {
+    clearInterval(this.myInterval);
   }
 
   render() {
     return (
-      <div id={'line' + this.props.id} className="epoch category10" style={{height: '400px', width: '100%'}}></div>
+      <div>
+        <div id={'flotplaceholder' + this.props.id} style={{width: '100%', height: '400px'}}></div>
+      </div>
     )
   }
 }
